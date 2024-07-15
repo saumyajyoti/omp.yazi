c52277f2abdaa9135ad2224592e682fa4879a1ed
三咲雅 · Misaki Masa <sxyazi@gmail.com>
2024-07-15 22:51:57 +0800

    feat: adapt to https://github.com/sxyazi/yazi/pull/1257

--- init.lua
+++ init.lua
@@ -1,6 +1,3 @@
--- async mode
--------------------------------------------------------------------------------
-
 local save = ya.sync(function(st, cwd, output)
 	if cx.active.current.cwd == Url(cwd) then
 		st.output = output
@@ -10,46 +7,32 @@ end)
 
 return {
 	setup = function(st)
-		Header.cwd = function()
+		Header:children_remove(1, Header.LEFT)
+		Header:children_add(function() return ui.Line.parse(st.output or "") end, 1000, Header.LEFT)
+
+		ps.sub("cd", function()
 			local cwd = cx.active.current.cwd
 			if st.cwd ~= cwd then
 				st.cwd = cwd
-				ya.manager_emit("plugin", { st._name, args = ya.quote(tostring(cwd), true) })
+				ya.manager_emit("plugin", { st._id, args = ya.quote(tostring(cwd), true) })
 			end
-
-			return ui.Line.parse(st.output or "")
-		end
+		end)
 	end,
 
 	entry = function(_, args)
 		local output = Command("oh-my-posh")
-				:args({
-					"print",
-					"primary",
-					"--no-status",
-					"--no-exit-code",
-					"-c",
-					"https://raw.githubusercontent.com/saumyajyoti/omp.yazi/main/yazi-prompt.omp.json",
-				})
-				:cwd(args[1])
-				:output()
+			:args({
+				"print",
+				"primary",
+				"--no-status",
+				"--no-exit-code",
+				"-c",
+				"https://raw.githubusercontent.com/saumyajyoti/omp.yazi/main/yazi-prompt.omp.json",
+			})
+			:cwd(args[1])
+			:output()
 		if output then
 			save(args[1], output.stdout:gsub("^%s+", ""))
 		end
 	end,
 }
-
----------------------------------------------------------------------------------------
--- sync mode
----------------------------------------------------------------------------------------
--- return {
--- setup = function(st)
--- Header.cwd = function()
--- local cwd = cx.active.current.cwd
--- if st.cwd ~= cwd then
--- st.cwd, st.output = cwd, io.popen("oh-my-posh print primary"):read("*a"):gsub("^%s+", "")
--- end
--- return ui.Line.parse(st.output)
--- end
--- end,
--- }
